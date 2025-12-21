a8daa85fa21c0c7cbf0a06722dc281b2d1fc2338
nikhilShakya7 <rsnikhil77@gmail.com>
2025-12-21 11:23:11 +0545

    initial commit

---
+++ Email Simulator/email_simulator.py
@@ -0,0 +1,24 @@
+class Email:
+    def __init__(self,sender,receiver,subject,body):
+        self.sender=sender
+        self.receiver=receiver
+        self.subject=subject
+        self.body=body
+
+    def mark_as_read(self):
+        self.read=True
+
+class User:
+    def __init__(self,name):
+        self.name=name
+        self.inbox=Inbox()
+
+        def send_email(self,receiver,subject,body):
+            email=Email(sender=self,receiver=receiver,subject=subject,body=body)
+            receiver.inbox.receive_email(email)
+
+class Inbox:
+    def __init__(self):
+        self.emails=[]
+    def receive_emails(self,email):
+        self.emails.append(email)
